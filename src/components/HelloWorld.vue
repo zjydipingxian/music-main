41bdf7abe2d059d6fb3b4807f6f4ca540d6e7e5e
zhongjiayao <1036432890@qq.com>
2021-04-10 00:56:32 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
-          >babel</a
+          >ba1bel</a
         >
       </li>
       <li>
