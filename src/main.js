552d07448eb36d8f3640f73c34481541a137b482
zhongjiayao <1036432890@qq.com>
2021-04-10 00:34:11 +0800

    提交第一版本代码

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+// 移动端适配
+import 'lib-flexible/flexible.js'
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
+
+createApp(App).use(store).use(router).mount('#app')
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, dir)
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete('preload') // TODO: need test
+    config.plugins.delete('prefetch') // TODO: need test
+
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('api', resolve('src/api'))
+      .set('views', resolve('src/views'))
+      .set('components', resolve('src/components'))
+  }
+}
